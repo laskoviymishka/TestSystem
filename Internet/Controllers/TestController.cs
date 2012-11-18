e30307c12b7f702a9f3dda925c04d3134c374791
laskoviymishka <laskoviymishka@gmail.com>
2012-11-18 13:57:39 +0300

    Тест контроллер

--- Internet/Controllers/TestController.cs
+++ Internet/Controllers/TestController.cs
@@ -9,6 +9,7 @@ namespace Internet.Controllers
 {
     public class TestController : Controller
     {
+        TestService _service;
         //
         // GET: /Test/
         [Authorize]
@@ -22,5 +23,35 @@ namespace Internet.Controllers
             return View(te.Tests.ToList());
         }
 
+
+        [Authorize(Roles = "Professor")]
+        public ActionResult Edit(int ID) 
+        {
+            TestEntities te = new TestEntities();
+            return View((Test)te.Tests.Where<Test>(t => t.ID == ID).FirstOrDefault<Test>());
+        }
+        [Authorize(Roles = "Professor")]
+        [HttpPost]
+        public ActionResult Edit(Test item)
+        {
+            _service = new TestService();
+            _service.UpdateItem(_service.GetByID(1),item);
+            return View();
+        }
+
+
+        [Authorize(Roles = "Professor")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [Authorize(Roles = "Professor")]
+        [HttpPost]
+        public ActionResult Create(Test item)
+        {
+            _service = new TestService();
+            _service.CreateItem(item);
+            return View();
+        }
     }
 }
